fc9374c4a94eeb174bfe2466628569ab909f73ce
Matias <matiasrsolis@yahoo.com.ar>
2020-10-22 14:34:22 -0300

    Product Detail

--- client/src/data.js
+++ client/src/data.js
@@ -1,48 +1,53 @@
 export default {
     products:[{
-        _id: 1,
+        _id: 1234,
         name: 'Variable product 01',
         price: 43.00,
         brand: 'OyM',
         image: '/images/cartera.jpg',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
     },
     {
-        _id: 2,
+        _id: 2345,
         name: 'Variable product 02',
         price: 43.00,
         brand: 'OyM',
         image: '/images/cartera.jpg',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
     },
     {
-        _id: 3,
+        _id: 3456,
         name: 'Variable product 03',
         price: 43.00,
         brand: 'OyM',
         image: '/images/cartera.jpg',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
     },
     {
-        _id: 4,
+        _id: 4567,
         name: 'Variable product 04',
         price: 43.00,
         brand: 'OyM',
         image: '/images/cartera.jpg',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
     },
     {
-        _id: 5,
+        _id: 5678,
         name: 'Variable product 05',
         price: 43.00,
         brand: 'OyM',
         image: '/images/cartera.jpg',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
     }
 ]
 }
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import data from '../data';
+import { Link } from 'react-router-dom';
 
 function ProductScreen(props){
+    let nuevoArray = []
+    for(var i=0; i<data.products.length; i++){        
+        var idDefinido = parseInt(props.match.params.id)
+        if(data.products[i]._id === idDefinido){
+            nuevoArray.push(data.products[i])
+        }
+    }
+    const product = nuevoArray[0]
+
     return(
         <div>
-            <h2>Product Screen</h2>
+            <div>
+                <Link to="/">Volver</Link>
+            </div>
+            <figure>
+                <img src={product.image} alt={product.name} />
+            </figure>
+            <div>
+                <ul>
+                    <h1>{product.name}</h1>
+                    <li><h2>{product.brand}</h2></li>
+                    <li><p>{`$ ${product.price}`}</p></li>
+                    <li><div className="precio">{product.rating}Stars({product.numReiews})</div></li>
+                    <li><p>{product.description}</p></li>
+                </ul>
+            </div> 
+            <aside className="details-action">
+                <ul>
+                    <li>
+                        Precio: {product.price}
+                    </li>
+                    <li>
+                        Status: {product.status}
+                    </li>
+                    <li>
+                        Cantidad: <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </select>
+                    </li>
+                    <button>Agregar al carro</button>
+                </ul>
+            </aside> 
         </div>
     );
 }
