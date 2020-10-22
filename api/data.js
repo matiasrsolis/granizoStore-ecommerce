77a3b771d3e12cf2be1fec47bcd4f166e0899960
Matias <matiasrsolis@yahoo.com.ar>
2020-10-22 14:51:35 -0300

    Creando BackEnd

---
+++ api/data.js
@@ -0,0 +1,53 @@
+export default {
+    products:[{
+        _id: 1234,
+        name: 'Variable product 01',
+        price: 43.00,
+        brand: 'OyM',
+        image: '/images/cartera.jpg',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
+    },
+    {
+        _id: 2345,
+        name: 'Variable product 02',
+        price: 43.00,
+        brand: 'OyM',
+        image: '/images/cartera.jpg',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
+    },
+    {
+        _id: 3456,
+        name: 'Variable product 03',
+        price: 43.00,
+        brand: 'OyM',
+        image: '/images/cartera.jpg',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
+    },
+    {
+        _id: 4567,
+        name: 'Variable product 04',
+        price: 43.00,
+        brand: 'OyM',
+        image: '/images/cartera.jpg',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
+    },
+    {
+        _id: 5678,
+        name: 'Variable product 05',
+        price: 43.00,
+        brand: 'OyM',
+        image: '/images/cartera.jpg',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'De buen cuero de color marrón y de calidad.'
+    }
+]
+}
