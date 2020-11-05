fdb89d41ce7cdcd119f03fd5b11e3a5be3c6693a
Matias <matiasrsolis@yahoo.com.ar>
2020-11-05 15:22:14 -0300

    Redux

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,50 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+
+
 
 function HomeScreen(props){
 
-    const [products, setProduct] = useState([]);
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const {data} = await axios.get("/api/products");
-            setProduct(data);
-        }
-        fetchData();
-        return() => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+      dispatch(listProducts());
 
-        }
-    }, [])
+      return() => {
 
-    return <ul>
-        {
-            products.map(product => 
-            <li key={product._id} className="productCard">
-                <div>
-                        <figure>
-                        <Link to={'/product/' + product._id}>
-                            <img
-                                className="product-image"
-                                src={product.image}
-                                alt="product"
-                            />
-                        </Link>
-                        </figure>
-                        <div className="productInfo">
-                            <Link to={'/product/' + product._id}>{product.name}</Link>
-                            <p className="marca">{product.brand}</p>
-                            <p className="precio">{product.price}</p>
-                            <div className="precio">{product.rating}Stars({product.numReiews})</div>
-                        </div>
-                </div>
-                    
-            </li> 
-            )
-        }
+      }
+  }, []);
+
+  
+  return (
+      <>
         
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <ul className="products">
+            {products.map((product) => (
+              <li key={product._id}>
+                <div className="product">
+                  <Link to={'/product/' + product._id}>
+                    <img
+                      className="product-image"
+                      src={product.image}
+                      alt="product"
+                    />
+                  </Link>
+                  <div className="product-name">
+                    <Link to={'/product/' + product._id}>{product.name}</Link>
+                  </div>
+                  <div className="product-brand">{product.brand}</div>
+                  <div className="product-price">${product.price}</div>
+                  <div className="product-rating">
+                    
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </>
+    );
 
-    </ul>
+
 }
 
 export default HomeScreen;
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import data from '../data';
+import {useDispatch, useSelector} from 'react-redux';
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../actions/productActions';
 
 function ProductScreen(props){
-    let nuevoArray = []
-    for(var i=0; i<data.products.length; i++){        
-        var idDefinido = parseInt(props.match.params.id)
-        if(data.products[i]._id === idDefinido){
-            nuevoArray.push(data.products[i])
-        }
-    }
-    const product = nuevoArray[0]
+    // let nuevoArray = []
+    // for(var i=0; i<data.products.length; i++){        
+    //     var idDefinido = parseInt(props.match.params.id)
+    //     if(data.products[i]._id === idDefinido){
+    //         nuevoArray.push(data.products[i])
+    //     }
+    // }
+    // const product = nuevoArray[0]
 
-    return(
-        <div>
+    const productDetails = useSelector(state => state.productDetails);
+    const {product, loading, error} = productDetails;
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(detailsProduct(props.match.params.id));
+        return() => {
+            //
+        };
+    }, [])
+
+    return <div>
             <div>
                 <Link to="/">Volver</Link>
             </div>
-            <figure>
+            {loading ?  <div>Loading...</div> :
+            error ? <div>{error}</div>:
+            
+            (<div>
+                <figure>
                 <img src={product.image} alt={product.name} />
             </figure>
             <div>
@@ -48,8 +64,11 @@ function ProductScreen(props){
                     <button>Agregar al carro</button>
                 </ul>
             </aside> 
+            </div>)}
+
+            
         </div>
-    );
+    
 }
 
 export default ProductScreen;
