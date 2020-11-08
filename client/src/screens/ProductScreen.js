98a71b3b11b10996b9cb6e28390f1233ef85b70d
Matias <matiasrsolis@yahoo.com.ar>
2020-11-08 15:20:13 -0300

    State products

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,74 +1,66 @@
-import React, { useEffect } from 'react';
-import data from '../data';
-import {useDispatch, useSelector} from 'react-redux';
-import { Link } from 'react-router-dom';
-import { detailsProduct } from '../actions/productActions';
+// import React, { useEffect } from 'react';
+// import data from '../data';
+// import {useDispatch, useSelector} from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import { detailsProduct } from '../actions/productActions';
 
-function ProductScreen(props){
-    // let nuevoArray = []
-    // for(var i=0; i<data.products.length; i++){        
-    //     var idDefinido = parseInt(props.match.params.id)
-    //     if(data.products[i]._id === idDefinido){
-    //         nuevoArray.push(data.products[i])
-    //     }
-    // }
-    // const product = nuevoArray[0]
+// function ProductScreen(props){
 
-    const productDetails = useSelector(state => state.productDetails);
-    const {product, loading, error} = productDetails;
-    const dispatch = useDispatch();
+//     const productDetails = useSelector(state => state.productDetails);
+//     const {product, loading, error} = productDetails;
+//     const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(detailsProduct(props.match.params.id));
-        return() => {
-            //
-        };
-    }, [])
+//     useEffect(() => {
+//         dispatch(detailsProduct(props.match.params.id));
+//         return() => {
+//             //
+//         };
+//     }, [])
 
-    return <div>
-            <div>
-                <Link to="/">Volver</Link>
-            </div>
-            {loading ?  <div>Loading...</div> :
-            error ? <div>{error}</div>:
+//     return <div>
+//             <div>
+//                 <Link to="/">Volver</Link>
+//             </div>
+//             {loading ?  <div>Loading...</div> :
+//             error ? <div>{error}</div>:
             
-            (<div>
-                <figure>
-                <img src={product.image} alt={product.name} />
-            </figure>
-            <div>
-                <ul>
-                    <h1>{product.name}</h1>
-                    <li><h2>{product.brand}</h2></li>
-                    <li><p>{`$ ${product.price}`}</p></li>
-                    <li><div className="precio">{product.rating}Stars({product.numReiews})</div></li>
-                    <li><p>{product.description}</p></li>
-                </ul>
-            </div> 
-            <aside className="details-action">
-                <ul>
-                    <li>
-                        Precio: {product.price}
-                    </li>
-                    <li>
-                        Status: {product.status}
-                    </li>
-                    <li>
-                        Cantidad: <select>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                        </select>
-                    </li>
-                    <button>Agregar al carro</button>
-                </ul>
-            </aside> 
-            </div>)}
+//             (<div>
+//                 <figure>
+//                 <img src={product.image} alt={product.name} />
+//             </figure>
+//             <div>
+//                 <ul>
+//                     <h1>{product.name}</h1>
+//                     <li><h2>{product.brand}</h2></li>
+//                     <li><p>{`$ ${product.price}`}</p></li>
+//                     <li><div className="precio">{product.rating}Stars({product.numReiews})</div></li>
+//                     <li><p>{product.description}</p></li>
+//                 </ul>
+//             </div> 
+//             <aside className="details-action">
+//                 <ul>
+//                     <li>
+//                         Precio: {product.price}
+//                     </li>
+//                     <li>
+//                         Status: {product.status}
+//                     </li>
+//                     <li>
+//                         Cantidad: <select>
+//                             <option>1</option>
+//                             <option>2</option>
+//                             <option>3</option>
+//                             <option>4</option>
+//                         </select>
+//                     </li>
+//                     <button>Agregar al carro</button>
+//                 </ul>
+//             </aside> 
+//             </div>)}
 
             
-        </div>
+//         </div>
     
-}
+// }
 
-export default ProductScreen;
+// export default ProductScreen;
--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,12 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import productListReducer from './reducers/productReducers';
-import productDetailsReducer from './reducers/productReducers';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';  //para retrasar el envío de una acción, o para enviar solo si se cumple una determinada condición. 
+import { productListReducer } from './reducers/productReducer';
 
 const initialState = {};
 const reducer = combineReducers({
-    productList: productListReducer,
-    productDetails: productDetailsReducer
+  productList: productListReducer,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    reducer,
-    initialState,
-    composeEnhancer(applyMiddleware(thunk))
-  );
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
