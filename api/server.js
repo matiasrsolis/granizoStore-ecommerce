a47f083eb3abd4b090e380091bff4aba6329cfd9
Matias <matiasrsolis@yahoo.com.ar>
2021-01-03 15:55:46 -0300

    Upload

--- api/server.js
+++ api/server.js
@@ -47,4 +47,6 @@ app.get("/api/products", (req, res) => {
 //   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 // });
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") })
+//app.listen(5000, () => { console.log("Server started at http://localhost:5000") })
+
+app.listen((process.env.PORT || 5000), function(){ console.log('listening on *:5000'); });
